dd8cb69805bbe1365b7a47086206ec8067f24be4
amesiastw <amesias@infoarmor.com>
2020-04-20 07:08:17 -0500

    creates new interface

--- src/com/pluralsight/calcengine/Adder.java
+++ src/com/pluralsight/calcengine/Adder.java
@@ -3,7 +3,7 @@ package com.pluralsight.calcengine;
 /**
  * Created by Jim on 10/10/2015.
  */
-public class Adder extends CalculateBase {
+public class Adder extends CalculateBase implements MathProcessing{
     public Adder(){}
     public Adder(double leftVal, double rightVal){
         super(leftVal, rightVal);
@@ -15,4 +15,21 @@ public class Adder extends CalculateBase {
         setResult(value);
     }
 
+    @Override
+    public String getKeyWord() {
+        return "add";
+    }
+
+    @Override
+    public char getSymbol() {
+        return '+';
+    }
+
+    @Override
+    public double doCalculation(double leftVal, double rightVal) {
+        setLeftVal(leftVal);
+        setRightVal(rightVal);
+        calculate();
+        return getResult();
+    }
 }
